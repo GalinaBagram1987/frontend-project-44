8f3d0b570434d4802255bfef57e4fbf4e4e8fc35
GalinaBagram1987 <gaby_ev@mail.ru>
2024-11-09 21:27:02 +1000

    fin-fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const greetings = () => {
-  const name = readlineSync.question("May I have your name? ");
-  let hello = `Hello, ${name}!`;
+  const name = readlineSync.question('May I have your name? ');
+  const hello = `Hello, ${name}!`;
   console.log(hello);
 };
 export default greetings;
