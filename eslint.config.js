0195eb0d7e2cb554e440d9469d49541111be70dd
GalinaBagram1987 <gaby_ev@mail.ru>
2024-11-26 17:02:39 +1000

    change eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import globals from 'globals';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+// import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,7 +9,14 @@ import pluginJs from '@eslint/js';
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+// const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
+
+const options = {
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended
+};
+
+const compat = new FlatCompat(options);
 
 export default [
   { languageOptions: { globals: globals.node } },
