bbec826a52b7f12fa6ab600bbfabe4ff967cf8b9
GalinaBagram1987 <gaby_ev@mail.ru>
2024-11-30 00:26:21 +1000

    correct index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import readlineSync from 'readline-sync';
+import { questionGame, gameRules } from './games/even.js';
 
-export const greetings = () => {
+const playGame = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  const hello = `Hello, ${name}!`;
-  console.log(hello);
-  return name;
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log(questionGame);
+
+  for (let i = 3; i > 0; i -= 1) {
+    const [question, currentAnswer] = gameRules();
+    console.log(`Question: ${question}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    if (yourAnswer === currentAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${yourAnswer} is wrong answer ;(. Correct answer was ${currentAnswer}.'`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
 };
-export const questionGames = (questionGame) => {
-  console.log(`${questionGame}?`);
-};
-export const congratulations = (name) => {
-  console.log(`Congratulations, ${name}!`);
-};
+export default playGame;
