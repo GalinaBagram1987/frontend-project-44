132ae5cce7f6b1cf794581cf7f0a013dd2ca7385
GalinaBagram1987 <gaby_ev@mail.ru>
2024-12-02 21:18:14 +1000

    change calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,11 +9,20 @@ export const generateQuestion = () => {
   const j = Math.floor(Math.random() * operators.length);
   const question = `${left} ${operators[j]} ${right}`;
   return {
-    question, left, right, operator: operators[j] }
+    question,
+    left,
+    right,
+    operator: operators[j],
+  };
 };
 
 const questionAndAnswer = () => {
-  const { question, left, right, operator } = generateQuestion();
+  const {
+    question,
+    left,
+    right,
+    operator,
+  } = generateQuestion();
   let currentAnswer;
   switch (operator) {
     case '+':
