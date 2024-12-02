079f2123d1be6da6783d8b2fb477f6cf18b14818
GalinaBagram1987 <gaby_ev@mail.ru>
2024-12-02 16:11:04 +1000

    change progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,40 +1,31 @@
-import readlineSync from 'readline-sync';
-import { greetings, questionGames, congratulations } from '../index.js';
+import randomNumber from '../utility.js';
+import playGame from '../index.js';
 
-const playerName = greetings();
-questionGames('What number is missing in the progression');
-const progression = () => {
-  for (let i = 3; i > 0; i -= 1) {
-    const arrayNumber = () => {
-      let startNumber = Math.ceil(Math.random() * 100);
-      const multiplier = Math.ceil(Math.random() * 10);
-      const quantity = 10;
-      let j = 0;
-      const arrNumber = [];
-      while (j < quantity) {
-        arrNumber.push(startNumber);
-        startNumber += multiplier;
-        j += 1;
-      }
-      return arrNumber;
-    };
-    const answerArray = arrayNumber();
-    const randomIndex = Math.floor(Math.random() * 10);
-    const correctAnswer = answerArray[randomIndex];
+export const questionGame = 'What number is missing in the progression';
 
-    const hiddenElArr = answerArray;
-    hiddenElArr[randomIndex] = '..';
-    const questionArray = hiddenElArr.join(' ');
-    console.log(`Question: ${questionArray}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (correctAnswer === Number(answer)) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}'  is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
+const arrayNumbers = () => {
+  let startNumber = randomNumber();
+  const multiplier = Math.ceil(Math.random() * 10);
+  const quantity = 10;
+  let j = 0;
+  const arrNumber = [];
+  while (j < quantity) {
+    arrNumber.push(startNumber);
+    startNumber += multiplier;
+    j += 1;
   }
-  congratulations(playerName);
+  return arrNumber;
 };
-export default progression;
+export const questionAndAnswer = () => {
+  const answerArray = arrayNumbers();
+  const randomIndex = Math.floor(Math.random() * 10);
+  const currentAnswer = answerArray[randomIndex].toString();
+  const hiddenElArr = answerArray;
+  hiddenElArr[randomIndex] = '..';
+  const question = hiddenElArr.join(' ');
+  return [question, currentAnswer];
+};
+const progression = () => {
+  playGame(questionGame, questionAndAnswer);
+};
+export { progression };
