3cf516d8ad9be29d49d2deba9f41e7ccd8dcea54
GalinaBagram1987 <gaby_ev@mail.ru>
2024-12-10 00:08:03 +1000

    rec utility.js

--- src/utility.js
+++ src/utility.js
@@ -1,5 +1,5 @@
-const isRandomNumber = () => {
-  const result = Math.ceil(Math.random() * 10);
+const isRandomNumber = (min, max) => {
+  const result = Math.ceil((Math.random() * (max - min) + min));
   return result;
 };
 export default isRandomNumber;
